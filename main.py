a4ccb5308b6a325b9d43e3f615354a93045b2802
Jayvee N Mapote <101295973+JvMapote@users.noreply.github.com>
2022-06-09 14:43:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Selection sort
+def selection_sort(nums):
+    for i in range(4):
+        minpos = i
+        for j in range(i,5):
+            if nums[j] < nums[minpos]:
+                minpos = j
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+        print(nums)
+nums = [20, 12, 10, 15, 2]
+selection_sort(nums)
+print("\nSelection Sort: ")
+print(nums)
